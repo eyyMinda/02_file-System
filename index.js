37004eee44eaf7bcb2835a4adffc9ec221173981
eyyMinda <gamerx14x@gmail.com>
2022-06-07 21:19:41 +0300

    fileCreate

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-import {file} from './lib/file.js';
+import { file } from './lib/file.js';
 
+const user = {
+    name: 'Sarah',
+    age: 22,
+    isNice: true
+}
 
-
-file.create('users', 'test.json', {'a': 'benny', 'b': 'johnny'})
 console.log('Hello World!')
+
+
+const readStatus = await file.read('users', 'josh.json')
+if (readStatus) console.log(readStatus)
+
+const createStatus = await file.create('users', 'test.json', user)
+if (createStatus) console.log(createStatus)
+
+// const readStatus3 = await file.read('users', 'test.json')
+// if(readStatus3) console.log(readStatus3)
