ac6262b82a31956fdf56e410905ef5f44f0f180c
eyyMinda <gamerx14x@gmail.com>
2022-06-08 19:05:42 +0300

    create,read,update,delete

--- index.js
+++ index.js
@@ -6,14 +6,30 @@ const user = {
     isNice: true
 }
 
-console.log('Hello World!')
+/* IIFE
+(async() => {
+    ...
+})();
+*/
 
 
-const readStatus = await file.read('users', 'josh.json')
-if (readStatus) console.log(readStatus)
+const readStatus = await file.read('users', 'test.json');
+if (readStatus) console.log(readStatus);
 
-const createStatus = await file.create('users', 'test.json', user)
-if (createStatus) console.log(createStatus)
+const createStatus = await file.create('users', 'test.json', user);
+if (createStatus) console.log(createStatus);
 
-// const readStatus3 = await file.read('users', 'test.json')
-// if(readStatus3) console.log(readStatus3)
+const readStatus2 = await file.read('users', 'test.json');
+if (readStatus2) console.log(readStatus2);
+user.isNice = false;
+const updateStatus = await file.update('users', 'test.json', user);
+if (updateStatus) console.log(updateStatus);
+
+const readStatus3 = await file.read('users', 'test.json');
+if (readStatus3) console.log(readStatus3);
+
+const deleteStatus = await file.delete('users', 'test.json')
+if (deleteStatus) console.log(deleteStatus)
+
+const readStatus4 = await file.read('users', 'test.json');
+if (readStatus4) console.log(readStatus4);
